3f563843c73cc57de1cd83f59da10b2cb9351e23
gabii998 <gabiiascurra@gmail.com>
2018-05-24 22:50:33 -0300

    Agregado de tipografia y seteo de texto

--- main.cpp
+++ main.cpp
@@ -14,12 +14,33 @@ using namespace std;
  * 
  */
 //Resolucion,aún hay que trabajar en esto
-int ancho=1024;
-int alto=720;
+int alto=sf::VideoMode::getDesktopMode().height;
+int ancho=sf::VideoMode::getDesktopMode().width;
 
 int main(int argc, char** argv) {
-
-   sf::RenderWindow window(sf::VideoMode(ancho, alto), "Asteroids",sf::Style::Fullscreen);
+    //Setea la resolucion de la pantalla
+   sf::RenderWindow window(sf::VideoMode(ancho,alto), "Asteroids",sf::Style::Fullscreen);
+   
+   //Tipografia
+   sf::Font font;
+   if (!font.loadFromFile("font/Cave-Story.ttf")){
+       //Aquí hay que manejar el error
+   }
+   sf::Text titulo;
+   sf::Text tituloSinglePlayer;
+   
+   titulo.setFont(font);
+   tituloSinglePlayer.setFont(font);
+   
+   titulo.setString("Asteroids");
+   tituloSinglePlayer.setString("Juego individual");
+   
+   titulo.setCharacterSize(56);
+   
+   //Centrar texto en pantalla
+   sf::FloatRect textRec = titulo.getLocalBounds();
+   titulo.setOrigin(textRec.left + textRec.width/2.0f , textRec.top + textRec.height/2.0f);
+   titulo.setPosition(sf::Vector2f(window.getSize().x/2,window.getSize().y/2));
    
 
     while (window.isOpen())
@@ -32,7 +53,10 @@ int main(int argc, char** argv) {
         }
 
         window.clear();
+        window.draw(titulo);
+        window.draw(tituloSinglePlayer);
         window.display();
+        
     }
 
     return 0;
