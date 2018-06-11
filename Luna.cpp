97ac96fa271df5c9ce8f9d877595e5f521726581
juanjavito <juanjavito@gmail.com>
2018-06-11 16:53:56 -0300

    progreso luna falta añadir los creditos (pendiente)

--- Luna.cpp
+++ Luna.cpp
@@ -5,48 +5,62 @@
  */
 
 /* 
- * File:   Luna.cpp
- * Author: root
+ * File:   Asteroide.cpp
+ * Author: gabriel
  * 
- * Created on 10 de junio de 2018, 01:05
+ * Created on 2 de junio de 2018, 14:52
  */
 
-#include "Luna.h"
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <iostream>
 
+#include "Luna.h"
 #include "ConstantesGlobales.h"
-
-#include <time.h>
 #include <random>
 using namespace std;
-Luna::Luna(sf::Texture &texturaLuna) {
-   spriteLuna.setTexture(texturaLuna); 
-   this->generarOrigen();
-   
-    
+
+Luna::Luna(sf::Texture &texturaNave) {
+    spriteLuna.setTexture(texturaNave);
+    this->generarOrigen();
+    //Esto inicializa una semilla en base a la fecha,para evitar que se genere el mismo numero
+    spriteLuna.setPosition(x, y);
+    spriteLuna.setScale(0.8, 0.8);
 }
+
 void Luna::Mostrar(sf::RenderWindow &window) {
     window.draw(spriteLuna);
 }
 
+void Luna::ActualizarPosicion() {
+     srand(time(NULL));
+     int num = (100+rand()%((altoResolucion-99)-100));
+    spriteLuna.move(sf::Vector2f(movimientoX, movimientoY));
+    if(spriteLuna.getPosition().y >= num){
+    
+        movimientoY=0;
+    }
+    
+}
+void Luna::setMovimientoY(int movimientoY){
+    this->movimientoY = movimientoY;
+
+}
 
 void Luna::generarOrigen() {
 
-  y=0;
-  x=rand() % anchoResolucion;//Genera posicion aleatoria para el ancho
-movimientoY=3;  
-movimientoX=0;
+    movimientoX = 0;
+    y = 0;
+    x = (100+rand() % (anchoResolucion-100)-100); //Genera posicion aleatoria para el ancho
+   movimientoY = rand() %3 +1;
 }
-void Luna::verificarExistencia(int indice){
-    int posX=spriteLuna.getPosition().x;
-    int posY=spriteLuna.getPosition().y;
-    if(posX > anchoResolucion+100 || posX<0 || posY<0 || posY>altoResolucion+100){
-        indice =1;
-    }
-    else{
-        indice =0;
+
+void Luna::verificarExistencia(int indice, std::vector<Luna> &vector) {
+    int posX = spriteLuna.getPosition().x;
+    int posY = spriteLuna.getPosition().y;
+    if (posX > anchoResolucion + 100 || posX < 0 || posY < 0 || posY > altoResolucion + 100) {
+        vector.erase(vector.begin() + indice);
     }
 }
+
