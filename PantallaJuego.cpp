6f126112dd0f326dc73f8424dc18d0eb59908953
juanjavito <juanjavito@gmail.com>
2018-06-13 04:08:54 -0300

    vidas
    falta añadir los creditos (pendiente)

--- PantallaJuego.cpp
+++ PantallaJuego.cpp
@@ -21,6 +21,7 @@
 #include <unistd.h>
 #include <thread>
 #include <time.h>
+
 PantallaJuego::PantallaJuego() {
     fondo.loadFromFile("Imagenes/back.png"); //cargo la imagen de la carpeta
     texturaAsteroide.loadFromFile("Imagenes/asteroideGrande.png");
@@ -30,23 +31,23 @@ PantallaJuego::PantallaJuego() {
     texturaBala.loadFromFile("Imagenes/new_bullet.png");
     explosion.loadFromFile("Imagenes/explosion.png");
     texturaNaveReal.loadFromFile("Imagenes/pixil.png");
-    
+
     //Inicializacion de sonidos
     musicaFondo.openFromFile("musica/ZanderNoriega-DarkerWaves.wav");
     explosionNaveBuffer.loadFromFile("musica/DeathFlash.flac");
     explosionNaveSound.setBuffer(explosionNaveBuffer);
-   
-    
+
+
 }
 
 int PantallaJuego::Run(sf::RenderWindow &App) {
     //Variables
-    bool running=true;
-    bool pausa=false;
-    int puntaje=0;
+    bool running = true;
+    bool pausa = false;
+    int puntaje = 0;
     sf::Font font;
-    int cantidadAsteroides=0;
-    sf::Sprite sprite;//creo un sprite
+    int cantidadAsteroides = 0;
+    sf::Sprite sprite; //creo un sprite
     std::vector <Asteroide> asteroides;
     std::vector <Luna> lunas;
     sf::Clock syncronice_timer;
@@ -54,7 +55,7 @@ int PantallaJuego::Run(sf::RenderWindow &App) {
     std::vector <Bala> balas;
     sf::Clock relojLuna;
     sf::Time timeLuna;
-    float tiempoLuna=0;
+    float tiempoLuna = 0;
     sf::Clock relojJuego;
     sf::Time timeJuego;
     sf::Clock relojDisparo;
@@ -64,22 +65,23 @@ int PantallaJuego::Run(sf::RenderWindow &App) {
     int tiempoSigNivel = 45;
     sf::Clock relojLuna2;
     sf::Time timeLuna2;
-    bool exploto=false;
-    int explotoEntero=0;
+    bool exploto = false;
+    int explotoEntero = 0;
+    int vidas=3;
     //Fin de declaracion de variables
-    
-//musica y sonidos
-     //Agregar credito a Zander Noriega pendiente
+
+    //musica y sonidos
+    //Agregar credito a Zander Noriega pendiente
     musicaFondo.setLoop(true);
     musicaFondo.play();
     musicaFondo.setVolume(45);
 
- //finMusica y sonidos 
-    Animacion explosionUno(explosion,1,1,256,256,48,0.3f);
+    //finMusica y sonidos 
+    Animacion explosionUno(explosion, 1, 1, 256, 256, 48, 0.3f);
     sprite.setTexture(fondo); //le seteo la textura
-     //seteo el tamaño del fondo
-    sprite.setOrigin(anchoResolucion/15,altoResolucion/5);
-    
+    //seteo el tamaño del fondo
+    sprite.setOrigin(anchoResolucion / 15, altoResolucion / 5);
+
     //Carga la tipografía
     if (!font.loadFromFile("font/Cave-Story.ttf")) {
         //Aquí hay que manejar el error
@@ -95,24 +97,24 @@ int PantallaJuego::Run(sf::RenderWindow &App) {
     score.setCharacterSize(32);
 
 
-    
-   
-    
+
+
+
     while (running) {
- 
-    timeJuego = relojJuego.getElapsedTime();
-    if((int)timeJuego.asSeconds() > tiempoSigNivel ){
-        velocidadAsteroide += 1;
-        relojJuego.restart();
-        tiempoSigNivel+=(velocidadAsteroide*5);
-    }
-        
-        
-    float delta_time_seconds = syncronice_timer.restart().asSeconds();
+
+        timeJuego = relojJuego.getElapsedTime();
+        if ((int) timeJuego.asSeconds() > tiempoSigNivel) {
+            velocidadAsteroide += 1;
+            relojJuego.restart();
+            tiempoSigNivel += (velocidadAsteroide * 5);
+        }
+
+
+        float delta_time_seconds = syncronice_timer.restart().asSeconds();
 
 
         //Actualiza el score en cada vuelta del bucle
-        score.setString("Score : "+ std::to_string(puntaje)+ "\nNivel de Velocidad    :  "+std::to_string(velocidadAsteroide+1)+"\nTiempo para el siguiente nivel    :     "+std::to_string(tiempoSigNivel-(int)timeJuego.asSeconds()));        //Esto es necesario para matar el programa cuando se cierra la ventana
+        score.setString("Score : " + std::to_string(puntaje) + "\nNivel de Velocidad    :  " + std::to_string(velocidadAsteroide + 1) + "\nTiempo para el siguiente nivel    :     " + std::to_string(tiempoSigNivel - (int) timeJuego.asSeconds())+"\nVidas : "+std::to_string(vidas)); //Esto es necesario para matar el programa cuando se cierra la ventana
 
         sf::Event event;
         while (App.pollEvent(event)) {
@@ -120,126 +122,137 @@ int PantallaJuego::Run(sf::RenderWindow &App) {
                 return (-1);
             }
         }
-        if( asteroides.size() <= 10){
-           
-            Asteroide asteroide=Asteroide(texturaAsteroide,velocidadAsteroide);
-            Asteroide asteroide2=Asteroide(texturaAsteroide,velocidadAsteroide);
-            Asteroide asteroide3=Asteroide(texturaAsteroide,velocidadAsteroide);
-            Asteroide asteroide4=Asteroide(texturaAsteroide2,velocidadAsteroide);
-            Asteroide asteroide5=Asteroide(texturaAsteroide3,velocidadAsteroide);
+        if (asteroides.size() <= 10) {
+
+            Asteroide asteroide = Asteroide(texturaAsteroide, velocidadAsteroide);
+            Asteroide asteroide2 = Asteroide(texturaAsteroide, velocidadAsteroide);
+            Asteroide asteroide3 = Asteroide(texturaAsteroide, velocidadAsteroide);
+            Asteroide asteroide4 = Asteroide(texturaAsteroide2, velocidadAsteroide);
+            Asteroide asteroide5 = Asteroide(texturaAsteroide3, velocidadAsteroide);
 
             asteroides.push_back(asteroide);
             asteroides.push_back(asteroide2);
             asteroides.push_back(asteroide3);
             asteroides.push_back(asteroide4);
             asteroides.push_back(asteroide5);
-            
-            cantidadAsteroides+=5;
+
+            cantidadAsteroides += 5;
         }
-        for(int i=0;i< asteroides.size();i++){
-            asteroides[i].verificarExistencia(i,asteroides);
+        for (int i = 0; i < asteroides.size(); i++) {
+            asteroides[i].verificarExistencia(i, asteroides);
         }
-         
-      if( lunas.size() < 1 && (velocidadAsteroide+1) >= 2 && (int)timeLuna2.asSeconds()>=10 ){ 
-            relojLuna.restart();           
-            Luna luna= Luna(texturaLuna);
+
+        if (lunas.size() < 1 && (velocidadAsteroide + 1) >= 2 && (int) timeLuna2.asSeconds() >= 10) {
+            relojLuna.restart();
+            Luna luna = Luna(texturaLuna);
             lunas.push_back(luna);
         }
-        timeLuna =relojLuna.getElapsedTime();
-        tiempoLuna=timeLuna.asSeconds();
-        if(tiempoLuna>30.0 ){
-           for(int i=0;i< lunas.size();i++){
-            lunas[i].setMovimientoY(2);
-          
-        }
-        }
-        if(lunas.size()>=1){
-        for(int i=0;i< lunas.size();i++){
-            
-           lunas[i].verificarExistencia(i,lunas,relojLuna2);
-        } }
-        timeLuna2=relojLuna2.getElapsedTime();
-        
-           nave.update(delta_time_seconds);
+        timeLuna = relojLuna.getElapsedTime();
+        tiempoLuna = timeLuna.asSeconds();
+        if (tiempoLuna > 30.0) {
+            for (int i = 0; i < lunas.size(); i++) {
+                lunas[i].setMovimientoY(2);
 
-        
+            }
+        }
+        if (lunas.size() >= 1) {
+            for (int i = 0; i < lunas.size(); i++) {
+
+                lunas[i].verificarExistencia(i, lunas, relojLuna2);
+            }
+        }
+        timeLuna2 = relojLuna2.getElapsedTime();
+
+        nave.update(delta_time_seconds);
+
+
         //limpiamos la pantalla
         App.clear();
         //dibujamos la pantalla
         App.draw(sprite); //dibujo fondo
-        
-         for(int i=0;i < lunas.size();i++){
-                lunas[i].Mostrar(App);
-                lunas[i].ActualizarPosicion();
-               // if(sf::Keyboard::isKeyPressed(sf::Keyboard::L))
-              //  {lunas[i].~Luna();}
-                
+
+        for (int i = 0; i < lunas.size(); i++) {
+            lunas[i].Mostrar(App);
+            lunas[i].ActualizarPosicion();
+            // if(sf::Keyboard::isKeyPressed(sf::Keyboard::L))
+            //  {lunas[i].~Luna();}
+
         }
         App.draw(score);
         nave.mostrar(App);
- 
-        timeDisparo=relojDisparo.getElapsedTime();
-        if(timeDisparo.asSeconds()>0.25){
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
-            Bala bala(nave, texturaBala);
-            balas.push_back(bala);
-            relojDisparo.restart(); 
+
+        timeDisparo = relojDisparo.getElapsedTime();
+        if (timeDisparo.asSeconds() > 0.25) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+                Bala bala(nave, texturaBala);
+                balas.push_back(bala);
+                relojDisparo.restart();
             }
         }
 
-        for(int i =0; i<balas.size() ;i++){
+        for (int i = 0; i < balas.size(); i++) {
             balas[i].mostrar(App);
             balas[i].colisiona(balas, asteroides, explosionUno, App, puntaje);
-            if(balas[i].spriteBala.getPosition().x > anchoResolucion && balas[i].spriteBala.getPosition().y > altoResolucion ){
-                balas.erase(balas.begin()+i);
+            if (balas[i].spriteBala.getPosition().x > anchoResolucion && balas[i].spriteBala.getPosition().y > altoResolucion) {
+                balas.erase(balas.begin() + i);
             }
         }
-                
-        for(int i=0;i < asteroides.size();i++){
+
+        for (int i = 0; i < asteroides.size(); i++) {
             asteroides[i].Mostrar(App);
             asteroides[i].ActualizarPosicion();
         }
-        nave.colisiona(asteroides, nave, explosionUno, App,exploto);
-        
-        if(exploto){
-            exploto=false;
+        nave.colisiona(asteroides, nave, explosionUno, App, exploto);
+
+        if (exploto) {
+            exploto = false;
             explosionNaveSound.play();
             explosionNaveSound.setVolume(300);
-        
+            nave.reset();
+            vidas--;
+            velocidadAsteroide=0;
+            tiempoSigNivel=45;
+            while (asteroides.begin() != asteroides.end()) {
+                asteroides.erase(asteroides.begin());
+            };
+            while (balas.begin() != balas.end()) {
+                balas.erase(balas.begin());
+            };
+            
         }
-        
-        
-        
-//        if(exploto && explotoEntero<=49){
-//            explotoEntero+=1;
-//                        explosionUno.mostrar(App,posicion);
-//                        explosionUno.actualizar();
-//                        if(explotoEntero>=43){                          
-//                            explotoEntero=0;
-//                            exploto=false;
-//                        }
-//        }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) && pausa){
+
+
+
+        //        if(exploto && explotoEntero<=49){
+        //            explotoEntero+=1;
+        //                        explosionUno.mostrar(App,posicion);
+        //                        explosionUno.actualizar();
+        //                        if(explotoEntero>=43){                          
+        //                            explotoEntero=0;
+        //                            exploto=false;
+        //                        }
+        //        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) && pausa) {
             musicaFondo.pause();
             std::vector<sf::Sprite> sprites;
             sprites.push_back(sprite);
             sprites.push_back(nave.getSprite());
-            for(int i=0;i<lunas.size();i++){
-                 sprites.push_back(lunas[i].getSprite());
+            for (int i = 0; i < lunas.size(); i++) {
+                sprites.push_back(lunas[i].getSprite());
             }
-           
-            
-            for(int i=0;i<asteroides.size();i++){
+
+
+            for (int i = 0; i < asteroides.size(); i++) {
                 sprites.push_back(asteroides[i].getSprite());
             }
             MenuPausa menuPausa;
-            menuPausa.run(App,sprites,running);
+            menuPausa.run(App, sprites, running);
             musicaFondo.play();
-            pausa=false;
+            pausa = false;
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) && !pausa){
-          
-            pausa=true;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) && !pausa) {
+
+            pausa = true;
         }
         App.display();
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num0)) {
